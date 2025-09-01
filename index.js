711fd4b15720f933cbaf6b7648e4222f7ecc057b
David <davidkuron1996@gmail.com>
2025-09-01 22:20:57 +0300

    adds this file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser.toUpperCase() + '!';
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+console.log(shortGreeting);
+console.log(excitedWelcomeMessage);
+console.log(welcomeMessage);
+console.log(currentUser);
+console.log(shortGreeting);
+console.log(shortGreeting);
+
+
+
+
+
+
